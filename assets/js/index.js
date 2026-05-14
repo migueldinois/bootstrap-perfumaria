490e4e614e9d1490f69eed70e81a727e2faafc26
migueldinois <miguel.dinois@aluno.senai.br>
2026-05-14 11:30:17 -0300

    feat: feito o javascript par a mandar notificacao de chamado quando for enviado o chamado, galeria de perfumes melhorada, e importacao do javascript nativo

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,24 @@
-// Alert funções
+// Pegando elementos do DOM
+
+const formChamado = document.querySelector('.contact__form')
+
+
+formChamado.addEventListener('submit', (event) => {
+    event.preventDefault();
+    Swal.fire({
+        title: "Deseja enviar o chamado?",
+        text: "Não será possivel apagar depois",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#bdf0e7",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Sim, enviar chamado",
+        cancelButtonText: "Cancelar chamado"
+    }).then((result) => {
+        if (result.isConfirmed) Swal.fire({
+            title: "Enviado!",
+            text: "Seu chamado foi enviado",
+            icon: "success"
+        });
+    });
+})
